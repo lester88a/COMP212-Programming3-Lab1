4f99e8e0ebe7e92827a94b4f66b34076f07eeee8
lester88a <lester88a@gmail.com>
2015-09-25 16:07:24 -0400

    array list example

--- COMP212  F15 Lab1 Li/Program.cs
+++ COMP212  F15 Lab1 Li/Program.cs
@@ -14,8 +14,6 @@ namespace COMP212__F15_Lab1_Li
             int currentlyCarsInStock = 0;
             int soldCars = 0;
             int totalSold = 0;
-            //int leftCars = 0;
-            //int reorderLevel = 0;
             
             //welcome messages
             Console.WriteLine("Welcome to Centennial College Car Reorder Program");
@@ -26,7 +24,6 @@ namespace COMP212__F15_Lab1_Li
             //New Counter Object, set the threshold reached at 80% of total
             Counter c = new Counter(Convert.ToInt32(currentlyCarsInStock * 0.8));
             c.ThresholdReached += c_ThresholdReached;
-            //c.ThresholdReached = c.ThresholdReached + c_ThresholdReached;
 
             
             while (true)
@@ -45,45 +42,11 @@ namespace COMP212__F15_Lab1_Li
                 }
             }
 
-
-            //Console.WriteLine("How many cars did Centennial College Car Delearship sell today?");
-            ////get the total stocked cars by user input
-            //do
-            //{
-            //    soldCars = Convert.ToInt32(Console.ReadLine());
-            //    soldCars++;
-            //if (soldCars >= currentlyCarsInStock)
-            //{
-            //    Console.WriteLine("Can't sell car more than the total car that in stock currently!");
-            //    Console.WriteLine("Try it again!");
-            //}
-            //    //leftCars = currentlyCarsInStock - soldCars;
-            //    //Console.WriteLine("left: {0}", leftCars);
-                
-            //} while (soldCars >= currentlyCarsInStock);
-
-            //leftCars = currentlyCarsInStock - soldCars;
-            ////set the reorder level 30% of total cars
-            //reorderLevel = Convert.ToInt32(leftCars * 0.3);
-            //Console.WriteLine("reorderLevel: {0}", reorderLevel);
-
-            //New Counter Object
-            //Counter c = new Counter(reorderLevel);
-            //c.ThresholdReached += c_ThresholdReached;
-
-            //while (leftCars > reorderLevel)
-            //{
-            //    c.Add(soldCars);
-            //}
         }
         static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
         {
-            //Console.WriteLine("The threshold of {0} was reached at {1}.",
-            //    e.Threshold, e.TimeReached);
             Console.WriteLine("The inventory reached the REORDER LEVEL: {0} at {1}. \nPlease order more cars now!"
                 , e.Threshold, e.TimeReached);
-            //Environment.Exit(0);
-
         }
     }
 }
